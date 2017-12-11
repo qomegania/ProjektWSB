e9f62fb8cf04dcf76c2781b713cbc743d1ed5852
qomegania <32014980+qomegania@users.noreply.github.com>
2017-12-11 21:13:23 +0100

    Upload Aplikacji Klienckiej
    Upload

---
+++ RCP_Client/RCP_Client/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RCP_Client
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string auth_string
+        {
+            get;
+            set;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string temp = textBox_login.Text + ";" + textBox_passwd;
+            string crypto_temp = temp;
+            auth_string = temp;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RCP_Client/RCP_Client/TCP_Operations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCP_Client
+{
+    static class TCP_Operations
+    {
+        public enum operations : int
+        {
+            LogIn = 0000,
+            GetList = 0001,
+            StartTask = 0010,
+            EndTask = 0011,
+            GetCommentList = 0100,
+            SendComment = 0101,
+            GetMessageList = 0110,
+            SendMessage = 0111,
+            GetUserList = 1000,
+            SendMyUserName = 1001,
+
+
+
+            LogOut = 1111
+
+
+        }
+
+    }
+}
